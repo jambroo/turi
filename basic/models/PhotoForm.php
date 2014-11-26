7e62204aad5d55d6c471bac581b091beabb26f1a
James Brooking <jambroo@gmail.com>
2014-11-26 20:42:02 +0100

    Init

---
+++ basic/models/PhotoForm.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\models;
+
+use yii\base\Model;
+
+class PhotoForm extends Model
+{
+	public $name;
+	public $width;
+	public $height;
+	
+	public function rules()
+	{
+		return [
+			[['name', 'width', 'height'], 'required'],
+			['email', 'email', 'height'],
+		];
+	}
+}
