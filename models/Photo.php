1fee22a64ff4283c99b7e96f1cfac989773ce6dc
James Brooking <jambroo@gmail.com>
2015-02-04 12:14:35 +0000

    Refactored photo model and added getters and setters.

--- models/Photo.php
+++ models/Photo.php
@@ -42,24 +42,54 @@ class Photo extends ActiveRecord
       ];
   }
 
-  public function setUrl()
+  // Helper function to get cloud front link for specific file
+  private function getCloudFrontLink($file, $expires = null)
   {
-		// Generate actual S3 Link
-		$config = Yii::$app->aws;
-	  $cloudFrontLinkViewHelperFactory = new CloudFrontLinkViewHelperFactory();
-	  $cloudFrontLinkViewHelper = $cloudFrontLinkViewHelperFactory->createService($config);
-	  $expires = time() + 10;
+    if ($expires === null) {
+      $expires = time() + 10;
+    }
 
-	  $this->url = $cloudFrontLinkViewHelper->__invoke($this->id, $config->config['cloudfront-key']['server'], $expires,
+    $config = Yii::$app->aws;
+    $cloudFrontLinkViewHelperFactory = new CloudFrontLinkViewHelperFactory();
+    $cloudFrontLinkViewHelper = $cloudFrontLinkViewHelperFactory->createService($config);
+
+    return $cloudFrontLinkViewHelper->__invoke($file, $config->config['cloudfront-key']['server'], $expires,
       $config->config['cloudfront-key']['key'],
       $config->config['cloudfront-key']['id']
     );
   }
 
+  public function setThumb()
+  {
+    $this->thumb = $this->getCloudFrontLink($this->id.'_t');
+  }
+
+  public function getThumb()
+  {
+    if (!isset($this->thumb)) {
+      $this->setThumb();
+    }
+    return $this->thumb;
+  }
+
+  public function setUrl()
+  {
+	  $this->url = $this->getCloudFrontLink($this->id);
+  }
+
+  public function getUrl()
+  {
+    if (!isset($this->url)) {
+      $this->setUrl();
+    }
+    return $this->url;
+  }
+
   public function load($data, $formName = null)
   {
   	if (parent::load($data, $formName)) {
-  		$this->setUrl();
+      $this->setUrl();
+  		$this->setThumb();
 
   		return true;
   	}
