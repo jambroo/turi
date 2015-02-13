d93886e7ef0a05a037a61f25e710a5ddc79c87f4
James Brooking <jambroo@gmail.com>
2015-02-13 15:08:12 +0000

    Added functional tests.

--- tests/codeception/functional/HomeCept.php
+++ tests/codeception/functional/HomeCept.php
@@ -5,7 +5,6 @@
 $I = new FunctionalTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
+$I->see('username');
+$I->see('password');
 $I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
