5ff382dfaaae1704f34c6782e30fb22b5d14d775
James Brooking <jambroo@gmail.com>
2015-02-13 15:05:10 +0000

    Adding acceptance tests.

--- tests/codeception/acceptance/HomeCept.php
+++ tests/codeception/acceptance/HomeCept.php
@@ -3,9 +3,6 @@
 /* @var $scenario Codeception\Scenario */
 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that home page works');
+$I->wantTo('ensure that home page redirs to login');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
-$I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
+$I->see('Login');
